d5adaeabd2498bac62c13a7b980814f6b1e1a574
Болоцкий Дмитрий Сергеевич <dbolotskiy@phoenixit.ru>
2025-12-04 10:17:35 +0300

    Kitchen order requests

--- kitchen-service/src/main/java/ru/Liga/kitchen/controller/KitchenController.java
+++ kitchen-service/src/main/java/ru/Liga/kitchen/controller/KitchenController.java
@@ -1,44 +1,44 @@
-//package ru.Liga.kitchen.controller;
-//
-//import org.springframework.web.bind.annotation.*;
-//import ru.Liga.kitchen.entity.KitchenOrder;
-//import ru.Liga.kitchen.service.KitchenService;
-//
-//import java.util.List;
-//
-//@RestController
-//@RequestMapping("/kitchen/dishes")
-//public class KitchenController {
-//
-//    private final KitchenService kitchenService;
-//
-//    public KitchenController(KitchenService kitchenService) {
-//        this.kitchenService = kitchenService;
-//    }
-//
-//    @GetMapping("/{id}")
-//    public KitchenOrder getById(@PathVariable Long id) {
-//        return kitchenService.getById(id);
-//    }
-//
-//    @GetMapping
-//    public List<KitchenOrder> getAll() {
-//        return kitchenService.getAll();
-//    }
-//
-//    @PostMapping
-//    public void create(@RequestBody KitchenOrder kitchenOrder) {
-//        kitchenService.create(kitchenOrder);
-//    }
-//
-//    @PutMapping("/{id}")
-//    public void update(@PathVariable Long id, @RequestBody KitchenOrder kitchenOrder) {
-//        kitchenOrder.setKitchen_order_id(id);
-//        kitchenService.update(kitchenOrder);
-//    }
-//
-//    @DeleteMapping("/{id}")
-//    public void delete(@PathVariable Long id) {
-//        kitchenService.delete(id);
-//    }
-//}
+package ru.Liga.kitchen.controller;
+
+import org.springframework.web.bind.annotation.*;
+import ru.Liga.kitchen.entity.KitchenOrder;
+import ru.Liga.kitchen.service.KitchenService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/kitchen/Order")
+public class KitchenController {
+
+    private final KitchenService kitchenService;
+
+    public KitchenController(KitchenService kitchenService) {
+        this.kitchenService = kitchenService;
+    }
+
+    @GetMapping("/{id}")
+    public KitchenOrder getById(@PathVariable Long id) {
+        return kitchenService.getById(id);
+    }
+
+    @GetMapping
+    public List<KitchenOrder> getAll() {
+        return kitchenService.getAll();
+    }
+
+    @PostMapping
+    public void create(@RequestBody KitchenOrder kitchenOrder) {
+        kitchenService.create(kitchenOrder);
+    }
+
+    @PutMapping("/{id}")
+    public void update(@PathVariable Long id, @RequestBody KitchenOrder kitchenOrder) {
+        kitchenOrder.setKitchen_order_id(id);
+        kitchenService.update(kitchenOrder);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        kitchenService.delete(id);
+    }
+}
--- kitchen-service/src/main/java/ru/Liga/kitchen/service/KitchenService.java
+++ kitchen-service/src/main/java/ru/Liga/kitchen/service/KitchenService.java
@@ -1,37 +1,37 @@
-//package ru.Liga.kitchen.service;
-//
-//import org.springframework.stereotype.Service;
-//import ru.Liga.kitchen.entity.KitchenOrder;
-//import ru.Liga.kitchen.mapper.KitchenOrderMapper;
-//
-//import java.util.List;
-//
-//@Service
-//public class KitchenService {
-//
-//    private final KitchenOrderMapper kitchenOrderMapper;
-//
-//    public KitchenService(KitchenOrderMapper kitchenOrderMapper) {
-//        this.kitchenOrderMapper = kitchenOrderMapper;
-//    }
-//
-//    public KitchenOrder getById(Long id) {
-//        return kitchenOrderMapper.findById(id);
-//    }
-//
-//    public List<KitchenOrder> getAll() {
-//        return kitchenOrderMapper.findAll();
-//    }
-//
-//    public void create(KitchenOrder kitchenOrder) {
-//        kitchenOrderMapper.insert(kitchenOrder);
-//    }
-//
-//    public void update(KitchenOrder kitchenOrder) {
-//        kitchenOrderMapper.update(kitchenOrder);
-//    }
-//
-//    public void delete(Long id) {
-//        kitchenOrderMapper.delete(id);
-//    }
-//}
+package ru.Liga.kitchen.service;
+
+import org.springframework.stereotype.Service;
+import ru.Liga.kitchen.entity.KitchenOrder;
+import ru.Liga.kitchen.mapper.KitchenOrderMapper;
+
+import java.util.List;
+
+@Service
+public class KitchenService {
+
+    private final KitchenOrderMapper kitchenOrderMapper;
+
+    public KitchenService(KitchenOrderMapper kitchenOrderMapper) {
+        this.kitchenOrderMapper = kitchenOrderMapper;
+    }
+
+    public KitchenOrder getById(Long id) {
+        return kitchenOrderMapper.findById(id);
+    }
+
+    public List<KitchenOrder> getAll() {
+        return kitchenOrderMapper.findAll();
+    }
+
+    public void create(KitchenOrder kitchenOrder) {
+        kitchenOrderMapper.insert(kitchenOrder);
+    }
+
+    public void update(KitchenOrder kitchenOrder) {
+        kitchenOrderMapper.update(kitchenOrder);
+    }
+
+    public void delete(Long id) {
+        kitchenOrderMapper.delete(id);
+    }
+}
